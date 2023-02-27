4582b8f10b145907c73f2168a6ad8fa2ca41968d
Ade Adeola <adedotster@gmail.com>
2023-02-27 17:48:30 +0000

    center CTA

--- containers/landing-page/CTA.tsx
+++ containers/landing-page/CTA.tsx
@@ -3,5 +3,4 @@ import styled from "styled-components";
 export default styled.div`
   color: white;
   text-shadow: 0 0 black;
-  margin-bottom: 25%;
 `;
--- containers/landing-page/StyledLanding.tsx
+++ containers/landing-page/StyledLanding.tsx
@@ -7,11 +7,12 @@ export default styled.div`
   background-position: 60%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 92%, 25% 100%, 0 94%);
   animation: ${backgroundShift} 20s linear 0s infinite forwards;
   background-repeat: repeat-x;
   background-size: 200% 100%;
   background-color: rgba(0, 0, 0, 0.7);
   background-blend-mode: color;
+  align-items: center;
 `;
