550d90aac6c9ac8dbe9aef04368b7ecc3873e6ab
Ade Adeola <adedotster@gmail.com>
2024-11-12 11:25:31 +0000

    remove min height

--- components/main/LandingWrapper.tsx
+++ components/main/LandingWrapper.tsx
@@ -4,7 +4,7 @@ export default function LandingWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center p-4 bg-gradient-to-br from-red-700 via-purple-500 to-teal-400 text-white">
+    <div className="flex flex-col justify-center items-center p-4 bg-gradient-to-br from-red-700 via-purple-500 to-teal-400 text-white">
       {children}
     </div>
   );
