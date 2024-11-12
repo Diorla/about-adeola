b9fa845c221e78621c86af2b3848f624c046d39b
Ade Adeola <adedotster@gmail.com>
2024-11-12 11:44:51 +0000

    remove static image

--- components/home/ServiceIcon.tsx
+++ components/home/ServiceIcon.tsx
@@ -1,30 +1,6 @@
-"use client";
 import Image from "next/image";
-import { createBreakpoint } from "react-use";
-
-const useBreakpoint = createBreakpoint({
-  sm: 640,
-  md: 768,
-  lg: 1024,
-  xl: 1280,
-  "2xl": 1536,
-});
 
 export default function ServiceIcon({ img }: { img: string }) {
-  const breakpoint = useBreakpoint();
-
-  if (breakpoint === "sm")
-    return (
-      <div className="flex items-center justify-center">
-        <Image
-          src={`/animations/${img}.gif`}
-          alt="Adeola"
-          width={80}
-          height={80}
-          unoptimized
-        />
-      </div>
-    );
   return (
     <div className="flex items-center justify-center">
       <Image
