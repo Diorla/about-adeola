0fabf582f11bcc81c4a811ebbe85b54ea775ec9e
Ade Adeola <adedotster@gmail.com>
2024-11-21 12:02:14 +0000

    contact form added

---
+++ app/contact/ContactForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import FadeInWhenVisible from "@/components/home/FadeInWhenVisible";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm, ValidationError } from "@formspree/react";
+import Image from "next/image";
+
+export function ContactForm() {
+  const [state, handleSubmit] = useForm("mqkglvzz");
+
+  if (state.succeeded) {
+    return (
+      <FadeInWhenVisible>
+        <div className="flex flex-col justify-center align-center w-full">
+          <h1 className="text-2xl font-bold text-center my-2">Message Sent!</h1>
+          <div className="flex justify-center">
+            <Image
+              src="/mailbox.png"
+              width={200}
+              height={200}
+              alt="mailbox"
+              className="margin-auto"
+            />
+          </div>
+          <p className="my-2">
+            Thanks for your message! I will try to respond at my earliest
+            convenience
+          </p>
+        </div>
+      </FadeInWhenVisible>
+    );
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="space-y-6 max-w-md margin-auto w-full"
+    >
+      <div>
+        <label htmlFor="name" className="block text-sm font-medium mb-1">
+          Name
+        </label>
+        <Input id="name" type="text" name="name" />
+      </div>
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium mb-1">
+          Email
+        </label>
+        <Input id="email" type="email" name="email" />
+      </div>
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <div>
+        <label htmlFor="message" className="block text-sm font-medium mb-1">
+          Message
+        </label>
+        <Textarea id="message" name="message" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Button type="submit" disabled={state.submitting}>
+          {state.submitting ? "Sending..." : "Send Message"}
+        </Button>
+      </div>
+    </form>
+  );
+}
+export default ContactForm;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,7 @@
 import Jumbotron from "@/components/main/Jumbotron";
 import type { Metadata } from "next";
+import ContactForm from "./ContactForm";
+import FadeInWhenVisible from "@/components/home/FadeInWhenVisible";
 
 export const metadata: Metadata = {
   title: "Contact",
@@ -9,6 +11,11 @@ export default function Contact() {
   return (
     <main>
       <Jumbotron title="Contact" subtitle="Welcome to the Contact Page" />
+      <FadeInWhenVisible>
+        <div className="flex justify-center my-4">
+          <ContactForm />
+        </div>
+      </FadeInWhenVisible>
     </main>
   );
 }
