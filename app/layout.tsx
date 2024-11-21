16a6d6b5167fee7d674c9f040495cff6e3d9156e
Ade Adeola <adedotster@gmail.com>
2024-11-21 20:22:45 +0000

    update maximum scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const viewport: Viewport = {
   themeColor: "#800000",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+  maximumScale: 10,
 };
 export const metadata: Metadata = {
   title: {
